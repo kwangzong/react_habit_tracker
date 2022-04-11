91320fe8ddff1411bf0bb2f3757a2182bc4b2520
naldawkj <naldawkj@gmail.com>
2022-04-11 23:52:31 +0900

    working header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => (
+  <header>
+    <img src="" alt="" className="logo" />
+    <div className="title">habit tracker</div>
+    <span className="count">0</span>
+  </header>
+);
+
+export default Header;
